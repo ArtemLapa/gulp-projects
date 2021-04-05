03408275bc085f29d3d71d6bd198fb6eb4d29790
ArtemLapa <artyom.lapa@gmail.com>
2021-04-05 19:21:19 +0300

    :lipstick: add burger menu

--- projects/smart/src/js/app.js
+++ projects/smart/src/js/app.js
@@ -1,6 +1,8 @@
-const menuBtn = document.querySelector('.header__menu-btn');
+const menuBtn = document.querySelector('.header__menu-btn'),
+  navMenuWrapper = document.querySelector('.header__navigation-menu-wrapper');
 
 menuBtn.addEventListener('click', function (event) {
   event.preventDefault;
   menuBtn.classList.toggle('header__menu-btn--active');
+  navMenuWrapper.classList.toggle('header__navigation-menu-wrapper--active');
 });
