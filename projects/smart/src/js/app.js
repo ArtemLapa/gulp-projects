0d51ff782abc3b290f75440deb7224fe8e9637b7
ArtemLapa <artyom.lapa@gmail.com>
2021-04-05 20:06:56 +0300

    :lipstick: add swiper-slider

--- projects/smart/src/js/app.js
+++ projects/smart/src/js/app.js
@@ -6,3 +6,32 @@ menuBtn.addEventListener('click', function (event) {
   menuBtn.classList.toggle('header__menu-btn--active');
   navMenuWrapper.classList.toggle('header__navigation-menu-wrapper--active');
 });
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+  arrows: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
+});
